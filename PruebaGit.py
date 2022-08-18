9464aeb82e076f1a47ebd890297da4c249465482
user <user>
2022-08-17 20:55:40 -0400

    Prueba GIT 1

--- PruebaGit.py
+++ PruebaGit.py
@@ -1,11 +1,11 @@
 import requests
 import json
-import config
+
 
 url_controller = "http://127.0.0.1:58000"
 
 #Obtenciòn del ticket
-data={config="username",config="password"}
+data={"password":"admin123!","username":"admin"}
 cabecera = {"content-type": "application/json"}
 respuesta = requests.post(url_controller+"/api/v1/ticket", json.dumps(data), headers=cabecera)
 token = (respuesta.json()["response"]["serviceTicket"])
